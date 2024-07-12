843db7861910b134b6c2637c91ce9f4f60eacff5
Pedrolpo <peixoto.plpo@gmail.com>
2024-07-12 13:41:20 -0300

    notas_faltas

---
+++ regression_model/regression_linear.py
@@ -0,0 +1,45 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+
+
+n = np.array([ 5.4, 9.5, 8.1, 6.8, 7.2 ])
+f = np.array([2, 4, 0, 2,0])
+def resul_cor():
+    corr_martrix = np.corrcoef(f,n)
+    corr = corr_martrix[0,1]
+    if corr==0:
+        print('Não há correlação nenhuma entre notas e faltas')
+    elif corr <0.05:
+        print('praticamente não há relação entre notas e faltas',corr)
+    elif corr<0.20:
+        print('relação muito fraca entre notas e faltas', corr)
+    elif corr<0.40:
+        print('relação fraca entre notas e faltas', corr)
+    elif corr<0.70:
+        print('relação forte entre notas e faltas' , corr)
+    elif corr<0.99:
+        print('relação muito forte entre notas e faltas' , corr)
+    elif corr==1:
+        print('Correlação perfeita positiva entre notas e faltas' , corr)
+    elif corr ==-1:
+        print('Correlação negativa perfeita entre notas e faltas', corr)
+
+resul_cor()
+
+
+
+f=f.reshape(5,1)
+
+modelo = LinearRegression()
+modelo.fit(f,n)
+
+c_ang = modelo.coef_
+intercp = modelo.intercept_
+
+plt.scatter(f,n)
+xreg = np.arange(0,16,1)
+plt.plot(xreg, c_ang*xreg + intercp )
+
+plt.show()
